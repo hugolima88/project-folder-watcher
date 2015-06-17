94cfdfa2d6f199dfafa7d5beeaf9198b464214f2
hugolima88 <hugolima88@gmail.com>
2015-06-16 22:18:25 -0400

    Code sync

---
+++ FolderWatcherClient/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace FolderWatcherClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbxFolderPath = new System.Windows.Forms.TextBox();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbxMessages = new System.Windows.Forms.TextBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.SuspendLayout();
+            // 
+            // tbxFolderPath
+            // 
+            this.tbxFolderPath.Location = new System.Drawing.Point(61, 21);
+            this.tbxFolderPath.Name = "tbxFolderPath";
+            this.tbxFolderPath.Size = new System.Drawing.Size(248, 20);
+            this.tbxFolderPath.TabIndex = 0;
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(315, 20);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(54, 20);
+            this.btnBrowse.TabIndex = 1;
+            this.btnBrowse.Text = "Browse";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(10, 24);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Folder:";
+            // 
+            // tbxMessages
+            // 
+            this.tbxMessages.Location = new System.Drawing.Point(13, 89);
+            this.tbxMessages.Multiline = true;
+            this.tbxMessages.Name = "tbxMessages";
+            this.tbxMessages.Size = new System.Drawing.Size(356, 241);
+            this.tbxMessages.TabIndex = 4;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(13, 61);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(356, 22);
+            this.btnStart.TabIndex = 5;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(381, 342);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.tbxMessages);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.tbxFolderPath);
+            this.Name = "Form1";
+            this.Text = "Folder Watcher - Client";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbxFolderPath;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbxMessages;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+    }
+}
+
---
+++ FolderWatcherClient/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FolderWatcherClient
+{
+    public partial class Form1 : Form
+    {
+        public string FOLDER_PATH { get; set; }
+        public FileSystemWatcher Watcher = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            tbxFolderPath.Text = FOLDER_PATH = @"C:\Temp";
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                tbxFolderPath.Text = FOLDER_PATH = folderBrowserDialog1.SelectedPath;
+            }
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            if (Watcher == null)
+            {
+                Watcher = new FileSystemWatcher();
+                Watcher.IncludeSubdirectories = true;
+                Watcher.Path = FOLDER_PATH;
+                Watcher.NotifyFilter = NotifyFilters.Attributes | NotifyFilters.CreationTime | NotifyFilters.DirectoryName | NotifyFilters.LastAccess | NotifyFilters.LastWrite | NotifyFilters.FileName;
+                Watcher.Filter = "*.*";
+                Watcher.Created += new FileSystemEventHandler(OnCreated);
+                Watcher.Renamed += new RenamedEventHandler(OnRenamed);
+                Watcher.Deleted += new FileSystemEventHandler(OnDeleted);
+                //Watcher.Changed += new FileSystemEventHandler(OnChanged);
+                Watcher.EnableRaisingEvents = true;
+
+                btnStart.Enabled = false;
+
+                tbxMessages.AppendText("Watching " + FOLDER_PATH + ".");
+            }
+        }
+
+        private void OnCreated(object source, FileSystemEventArgs e)
+        {
+            AppendMessage(Environment.NewLine + "CREATE: " + e.Name);
+        }
+
+        private void OnRenamed(object source, RenamedEventArgs e)
+        {
+            AppendMessage(Environment.NewLine + "RENAME: " + e.Name);
+        }
+
+        private void OnDeleted(object source, FileSystemEventArgs e)
+        {
+            AppendMessage(Environment.NewLine + "DELETE: " + e.Name);
+        }
+
+        //private void OnChanged(object source, FileSystemEventArgs e)
+        //{
+        //    AppendMessage(Environment.NewLine + "CHANGE: " + e.Name);
+        //}
+
+        delegate void AppendMessageDelegate(string message);
+
+        private void AppendMessage(string message)
+        {
+            if (this.tbxMessages.InvokeRequired)
+            {
+                AppendMessageDelegate d = new AppendMessageDelegate(AppendMessage);
+                this.Invoke(d, new object[] { message });
+            }
+            else
+            {
+                this.tbxMessages.AppendText(message);
+            }
+        }
+    }
+}
